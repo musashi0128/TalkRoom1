71ce83fff109c250f6a1a81413d0ba8f95d971ac
KazuhikoMiyamoto <k.miyamoto.0128@gmail.com>
2016-12-27 14:07:30 +0900

    Initial Commit

---
+++ TalkRoom1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TalkRoom1
+//
+//  Created by 宮本一彦 on 2016/12/27.
+//  Copyright © 2016年 宮本一彦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
